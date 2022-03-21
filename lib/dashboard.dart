eaeba4f3ad6b44b98c7ef195b05405da2f71960b
Sujanban <bansujan@gmail.com>
2022-03-21 19:46:02 +0545

    created dashboard

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:day6_login_signup_page/login.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatefulWidget {
@@ -11,15 +12,77 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.teal
-      ),
+      theme: ThemeData(primarySwatch: Colors.teal),
       home: Scaffold(
         appBar: AppBar(
-          automaticallyImplyLeading: false,
           title: const Text("Dashboard"),
         ),
-      ),
-    );
+        body: const Center(
+          child: Text("Drawer Home"),
+        ),
+        drawer: Drawer(
+            child: ListView(
+              padding: const EdgeInsets.all(0),
+              children: [
+                const UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/background.png"),
+                      fit: BoxFit.cover
+                    )
+                  ),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: AssetImage("assets/profile.jpg"),
+                    backgroundColor: Colors.green,
+                  ),
+                    accountName: Text("Sujan Ban",
+                      style: TextStyle(
+                      fontSize: 25
+                    ),),
+                    accountEmail: Text("bansujan@gmail.com"),
+                  arrowColor: Colors.green,
+                ),
+                ListTile(
+                  leading: const Icon(Icons.favorite),
+                  title: const Text("Favourite"),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: const Icon(Icons.share),
+                  title: const Text("Share"),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: const Icon(Icons.people),
+                  title: const Text("Friends"),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: const Icon(Icons.category),
+                  title: const Text("Category"),
+                  onTap: () {},
+                ),
+                const Divider(
+                  thickness: 2,
+                  indent: 20,
+                  endIndent: 20,
+                ),
+                ListTile(
+                  leading: const Icon(Icons.logout),
+                  title: const Text("Logout"),
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const MyLogin()));
+                  },
+                ),
+                const Divider(
+                  thickness: 2,
+                  indent: 20,
+                  endIndent: 20,
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
   }
 }
